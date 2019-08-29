6a2eb785fcde0d3fab6b6ae20fd39c6831635c96
Yonat Sharon <yonat@ootips.org>
2019-08-29 22:36:30 +0300

    better podspec

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ import PackageDescription
 let package = Package(
     name: "MockImagePicker",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v9),
     ],
     products: [
-        .library( name: "MockImagePicker", targets: ["MockImagePicker"])
+        .library(name: "MockImagePicker", targets: ["MockImagePicker"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/yonat/SweeterSwift", from: "1.0.2")
+        .package(url: "https://github.com/yonat/SweeterSwift", from: "1.0.2"),
     ],
     targets: [
-        .target(name: "MockImagePicker", dependencies: ["SweeterSwift"], path: "Sources")
+        .target(name: "MockImagePicker", dependencies: ["SweeterSwift"], path: "Sources"),
     ],
     swiftLanguageVersions: [.v5]
 )
